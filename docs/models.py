3e63f62075fe4a89a6d008153a25828d365ba79e
Yakhyokhan <yakhyokhanodilov@gmail.com>
2023-06-08 23:17:31 +0300

    Docs model created

--- docs/models.py
+++ docs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from page.models import Page
+from responsible_group.models import ResponsiblePerson
 
 # Create your models here.
+
+class Docs(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.PROTECT)
+    curr_responsible = models.ForeignKey(ResponsiblePerson, on_delete=models.PROTECT)
+    values = models.JSONField()
