1101d4725dd159e0f38b89abcd0e85f85f6e528c
Yakhyokhan <yakhyokhanodilov@gmail.com>
2023-06-22 19:54:19 +0300

    html models is register to admin

--- page/admin.py
+++ page/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Page
-from django.forms import ModelForm
+from .models import Page, Html
 
 
 # Register your models here.
 
 admin.site.register(Page)
+admin.site.register(Html)
