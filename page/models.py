1bd9bfdf9539241f8e619cc98f67f1762a5ecd36
Yakhyokhan <yakhyokhanodilov@gmail.com>
2023-06-22 19:52:59 +0300

    html is seperated from page and tag and inputs fields is made

--- page/models.py
+++ page/models.py
@@ -1,44 +1,66 @@
-from typing import Any, Iterable, Optional
 from django.db import models
 from responsible_group.models import ResponsibleGroup
 from company.models import Company
 from tags.serializer import AnyTagSerializer, ManyTagSerializer
 from tags.field_finders import AnyFieldFinder
+from typing import Any
 # Create your models here.
 
-class Page(models.Model):
+class TagField(models.JSONField):
+    db_writer = AnyTagSerializer
+    def from_db_value(self, value: Any, expression: Any, connection: Any) -> Any:
+        json = super().from_db_value(value, expression, connection)
+        if json == None: return None
+        return self.db_writer.create(**json).obj
+    def to_python(self, value: Any) -> Any:
+        json = super().to_python(value)
+        if json == None: return None
+        return self.db_writer.create(**json).obj
+    
+    def get_prep_value(self, value: Any) -> Any:
+        json = self.db_writer.create_with_obj(value).get_info()
+        return super().get_prep_value(json)
+    
+    
+    
+class ManyTagField(models.JSONField):
+    db_writer = ManyTagSerializer
+    def from_db_value(self, value: Any, expression: Any, connection: Any) -> Any:
+        json = super().from_db_value(value, expression, connection)
+        if json == None: return None
+        return self.db_writer.create(json).obj_list
+    def to_python(self, value: Any) -> Any:
+        json = super().to_python(value)
+        if json == None: return None
+        return self.db_writer.create(json).obj_list
+    
+    def get_prep_value(self, value: Any) -> Any:
+        db_obj = self.db_writer()
+        db_obj.add_many(value)
+        json = db_obj.get_info()
+        return super().get_prep_value(json)
+
+class Html(models.Model):
+    name = models.CharField(max_length=50)
+    tags = TagField()
+    inputs = ManyTagField(null=True, blank=True)
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        if self.html:
-            html = AnyTagSerializer.create(**self.html)
-            self.html = html.obj
-        inputs = self.inputs
-        if inputs != None: 
-            self.inputs = ManyTagSerializer.create(inputs).obj_list
-            return
-        self.inputs = AnyFieldFinder.find(self.html)
+        if self.inputs == None:
+            self.inputs = AnyFieldFinder.find(self.tags)
 
-    def save(self, *args, **kwargs) -> None:
-        html = self.html
-        self.html = AnyTagSerializer.create_with_obj(html).get_info()
-        inputs = AnyFieldFinder.find(html)
-        input_info = ManyTagSerializer()
-        input_info.add_many(inputs)
-        self.inputs = input_info.get_info()
-        super().save(*args, **kwargs)
-        self.html = html
-        self.inputs = inputs
+    def save(self, force_insert = False, force_update = False, using = None, update_fields = None):
+        self.inputs = AnyFieldFinder.find(self.tags)
+        if update_fields is not None and "inputs" in update_fields:
+            update_fields = {"inputs"}.union(update_fields)
+        super().save(force_insert, force_update, using, update_fields)
+    
 
+
+class Page(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=25)
-    html = models.JSONField()
+    html = models.ForeignKey(Html, on_delete=models.PROTECT, null=True, blank=True)
     responsible_group = models.ForeignKey(ResponsibleGroup, on_delete=models.PROTECT)
-    inputs = models.JSONField(null=True, blank=True)
-
-
-
-
-
-
 
