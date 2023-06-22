d8ff76c6c9875e96507706e466fabb8d43518b11
Yakhyokhan <yakhyokhanodilov@gmail.com>
2023-06-22 20:00:10 +0300

    html creat view is made

--- page/views.py
+++ page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializer import HtmlSerializer
+from rest_framework import generics
+from .models import Html
 
 # Create your views here.
+
+class HtmlCreateListView(generics.CreateAPIView):
+    queryset = Html.objects.all()
+    serializer_class = HtmlSerializer
